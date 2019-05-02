35ff9665bcbe6919dae3394553b654edcaf67e21
Tay <tayd93@hotmail.com>
2019-05-02 15:40:22 -0600

    Adds deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -7,7 +7,7 @@ require './lib/deck'
 class DeckTest < Minitest::Test
 
   def test_deck_exists
-    card_1= Card.new("What is the capital of Alaska", "Juneau", :Geography)
+    card_1 = Card.new("What is the capital of Alaska", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
@@ -15,6 +15,29 @@ class DeckTest < Minitest::Test
     deck = Deck.new(cards)
 
     assert_instance_of Deck, deck
+  end
+
+  def test_count_works
+    card_1 = Card.new("What is the capital of Alaska", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
+  end
+
+  def test_cards_in_category
+    card_1 = Card.new("What is the capital of Alaska", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_equal
+    #is there a way to test all of the categories at once or will i have to test for each specific category that i've created an instance for.
   end
 
 end
